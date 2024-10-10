68fbfad39d695c62c93e22c54bdfec5a47887cda
augustojerke <augustowieles@gmail.com>
2024-10-10 20:39:37 -0300

    ajustes

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ class GamePriceFinder:
     
     def searchKabum(self):
         kabum = Kabum()
-        kabum.searchProduct(searchParam, self.driver)
+        kabum.searchProduct(searchParam)
     
-    def searchAmazon(self):
-        amazon = Amazon()
-        amazon.searchProduct(searchParam, self.driver)
+    # def searchAmazon(self):
+    #     amazon = Amazon()
+    #     amazon.searchProduct(searchParam, self.driver)
 
 if __name__ == '__main__':
     gamePriceFinder = GamePriceFinder()
     gamePriceFinder.searchKabum()
-    gamePriceFinder.searchAmazon()
+    # gamePriceFinder.searchAmazon()
